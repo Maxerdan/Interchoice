27542b93c54f960c1828795c8529b46c919c72ec
Sokolov Max <maxerdan@yandex.ru>
2022-05-08 09:47:55 +0500

    swagger fix

--- Startup.cs
+++ Startup.cs
@@ -84,14 +84,14 @@ namespace Interchoice
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "InterChoice API");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("v1/swagger.json", "InterChoice API");
-                });
             }
             else
             {
